5389b4a9c67c0974040365b42cf250ad647470c3
kameoman <spiderman.935963@gmail.com>
2021-08-31 18:39:40 +0900

    検証

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Activation, Dense
 from keras.layers import Conv2D, MaxPooling2D, Flatten
+from keras.preprocessing.image import ImageDataGenerator
 
 def main():
     model = Sequential()
@@ -17,7 +18,30 @@ def main():
     model.add(Activation("relu"))
     model.add(Dense(2))
     model.add(Activation("softmax"))
-    model.summary()
+    # model.summary()
+
+    model.compile(
+            optimizer="adam",
+            # 分類に関して
+            loss="categorical_crossentropy",
+            metrics=["accuracy"])
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    train_generator = train_datagen.flow_from_directory(
+          "data/train",
+          target_size=(64,64),
+          batch_size=10)
+    validation_generator = test_datagen.flow_from_directory(
+      "data/validation",
+          target_size=(64,64),
+          batch_size=10)
+    model.fit_generator(
+            train_generator,
+            epochs=20,
+            steps_per_epoch=10,
+            validation_data=validation_generator,
+            validation_steps=10)
+    model.save("model.h5")
 
 
 if __name__ == "__main__":
