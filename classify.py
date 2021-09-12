41b783ae5de6a8b8d9c196b428fc0cc0e6814557
kameoman <spiderman.935963@gmail.com>
2021-09-12 12:33:47 +0900

    fix

--- classify.py
+++ classify.py
@@ -1,11 +1,14 @@
 import streamlit as st
+import tensorflow as tf
 import sys
+import keras
 from PIL import Image
 from keras.models import load_model
 import numpy as np
 
 uploaded_file = st.file_uploader("Choose an image...", type='jpg')
-st.image(uploaded_file)
+if uploaded_file is not None:
+  st.image(uploaded_file)
 
 def main():
   image = Image.open(uploaded_file)
